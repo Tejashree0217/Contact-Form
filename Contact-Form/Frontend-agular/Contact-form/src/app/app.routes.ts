6516eaadd5baea1f7c7e931495a88922ecd34465
Tejashree0217 <tejashreekalyanshetti@gmail.com>
2024-09-11 16:02:54 +0530

    example

---
+++ Contact-Form/Frontend-agular/Contact-form/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+export const routes: Routes = [
+
+  {path:'contact-form',component:ContactFormComponent}
+];
+
